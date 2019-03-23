e32785a8c1581c39deb55f6c15a48b168d8128c0
Wilko Manger <wilko@rens.onl>
2019-03-23 14:03:59 +0100

    Update Matrix SDK

--- lib/src/ui/start/start_bloc.dart
+++ lib/src/ui/start/start_bloc.dart
@@ -23,11 +23,37 @@ final auth = AuthenticationBloc();
 
 class AuthenticationBloc {
 
+  Homeserver _homeserver;
+
   final isUsernameAvailableSubj = BehaviorSubject<bool>();
   Observable<bool> get isUsernameAvailable => isUsernameAvailableSubj.stream;
 
   void checkUsernameAvailability(String username) {
-    var hs = homeserver(uri: Uri.parse("https://matrix.org"));
+    // Check if there is a ':' in the username,
+    // if so, treat it as a full Matrix ID (without or without '@').
+    // Otherwise use the local part (with or without '@').
+    // So, accept all of these formats:
+    // @joe:matrix.org
+    // joe:matrix.org
+    // joe
+    // @joe
+    if (username.contains(':')) {
+      var split = username.split(':');
+      username = split[0];
+      String server = split[1];
+
+      try {
+        var serverUri = Uri.parse(server);
+
+        _homeserver = homeserver(uri: serverUri);
+      } catch (FormatException) {
+        isUsernameAvailableSubj.addError(InvalidHostnameException());
+      }
+    }
+
+    if (username.startsWith('@')) {
+      username = username.substring(1);
+    }
 
     if (!Username.isValid(username)) {
       isUsernameAvailableSubj.addError(InvalidUsernameException());
@@ -39,3 +65,5 @@ class AuthenticationBloc {
     );
   }
 }
+
+class InvalidHostnameException implements Exception { }
