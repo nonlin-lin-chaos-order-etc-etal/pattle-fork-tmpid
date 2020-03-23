1f56162bbc63e7fc81072ac2dff061daf07274b1
Wilko Manger <wilko@rens.onl>
2020-03-23 22:07:40 +0100

    Added clarifying comments

--- generate_intl.sh
+++ generate_intl.sh
@@ -7,6 +7,9 @@ flutter pub run intl_translation:generate_from_arb \
    --output-dir=lib/src/resources/intl --no-use-deferred-loading \
    lib/src/resources/intl/localizations.dart lib/src/resources/intl/*.arb
 
+# The dartanalyzer exclude functionality currently does not work (see https://github.com/dart-lang/sdk/issues/25551),
+# therefore the generated files are formatted and have extra linter ignore statements added
+
 echo '// ignore_for_file: avoid_catches_without_on_clauses,type_annotate_public_apis,lines_longer_than_80_chars' \
 	| tee -a lib/src/resources/intl/messages*.dart
 
